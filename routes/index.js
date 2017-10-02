42770d2394a8c62cb7cbcc202e3c6f53d417d66d
Mark Cube <Mark Cube>
2017-10-03 01:17:24 +0800

    second commit

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,35 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', ensureAuthenticated, function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+function ensureAuthenticated(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	} else {
+		//req.flash('error_msg', 'You are not logged in');
+		res.redirect('/users/login');
+	}
+}
+
+function ensureLoggedIn(req, res, next){
+	if(req.isAuthenticated()){
+		req.flash('error_msg', 'You are already logged in');
+		res.redirect('/');
+	} else {
+		return next();	
+	}
+}
+
+function ensureLoggedInReg(req, res, next){
+	if(req.isAuthenticated()){
+		req.flash('error_msg', 'You cannot register while logged in');
+		res.redirect('/');
+	} else {
+		return next();
+	}
+}
 
 module.exports = router;

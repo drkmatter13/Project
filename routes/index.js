d31fc15a252249be85f6b4712a1206344a00cdb0
Mark Cube <Mark Cube>
2017-10-05 20:48:55 +0800

    added about page

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,16 @@ router.get('/', ensureAuthenticated, function(req, res, next) {
   res.render('index', { title: 'Project', user: req.user });
 });
 
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Project'});
+});
+
 function ensureAuthenticated(req, res, next){
 	if(req.isAuthenticated()){
 		return next();
 	} else {
 		//req.flash('error_msg', 'You are not logged in');
-		res.redirect('/users/login');
+		res.redirect('/about');
 	}
 }
 module.exports = router;
